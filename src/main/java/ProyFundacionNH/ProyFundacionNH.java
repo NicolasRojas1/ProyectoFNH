d9577256b0a962728a71c2a0491e1e8267cd755c
nortic97 <nortic97@gmail.com>
2022-06-28 19:16:30 -0500

    Restructuracion

--- src/main/java/ProyFundacionNH/ProyFundacionNH.java
+++ src/main/java/ProyFundacionNH/ProyFundacionNH.java
@@ -1,10 +1,14 @@
 
 package ProyFundacionNH;
 
+import ProyFundacionNH.view.Principal;
+
 public class ProyFundacionNH {
     
     public static void main(String[] args) {
         
-        System.out.println("hola mundo");
+        Principal form = new Principal();
+        form.setVisible(true); // llamar a la ventana Principal para desencadenar el programa.
+        
     }
 }
---
+++ src/main/java/ProyFundacionNH/controller/PersonasController.java
@@ -0,0 +1,23 @@
+
+package ProyFundacionNH.controller;
+
+import ProyFundacionNH.models.Personas;
+
+
+public class PersonasController {
+    
+    Personas persona = new Personas();
+    
+//    //public static void main(String[] args) {
+//     
+//    }
+//    
+//    private Personas mostrarPersona() {
+//
+//        persona.setPerId(0);
+//        persona.setPerNombres("Nicolas");
+//        persona.setPerApellidos("Rojas");
+//        persona.setPerBarrio("Gustavo Restrepo");
+//        return persona;
+//    }
+    }
